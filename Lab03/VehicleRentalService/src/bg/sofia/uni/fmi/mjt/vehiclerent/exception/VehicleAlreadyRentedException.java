05e37ef33b3917a74e4724b039a358cf3bef6b1a
Daniel Yordanov <62522030+danielgo26@users.noreply.github.com>
2025-03-27 16:13:16 +0200

    Delete Lab03/VehicleRentalService directory

--- Lab03/VehicleRentalService/src/bg/sofia/uni/fmi/mjt/vehiclerent/exception/VehicleAlreadyRentedException.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.vehiclerent.exception;
-
-public class VehicleAlreadyRentedException extends RuntimeException{
-    public VehicleAlreadyRentedException(String errorMessage){
-        super(errorMessage);
-    }
-}
--- Lab03/VehicleRentalService/src/bg/sofia/uni/fmi/mjt/vehiclerent/exception/VehicleNotRentedException.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.vehiclerent.exception;
-
-public class VehicleNotRentedException extends RuntimeException{
-    public VehicleNotRentedException(String errorMessage){
-        super(errorMessage);
-    }
-}
