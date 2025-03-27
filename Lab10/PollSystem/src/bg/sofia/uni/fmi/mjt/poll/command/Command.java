2d164db075c526668392407a4332bab8ee762cc6
Daniel Yordanov <62522030+danielgo26@users.noreply.github.com>
2025-03-27 16:14:05 +0200

    Delete Lab10 directory

--- Lab10/PollSystem/src/bg/sofia/uni/fmi/mjt/poll/command/Command.java
+++
@@ -1,4 +0,0 @@
-package bg.sofia.uni.fmi.mjt.poll.command;
-
-public record Command(String command, String[] arguments) {
-}
--- Lab10/PollSystem/src/bg/sofia/uni/fmi/mjt/poll/server/model/Poll.java
+++
@@ -1,5 +0,0 @@
-package bg.sofia.uni.fmi.mjt.poll.server.model;
-
-import java.util.Map;
-
-public record Poll(String question, Map<String, Integer> options) { }
